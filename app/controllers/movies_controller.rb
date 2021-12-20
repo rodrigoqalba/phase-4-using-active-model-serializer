34926c561d2a755c9797802bf20285e8553527d9
Rodrigo Alba <rodrigo@megabadvisors.com>
2021-12-19 20:36:53 -0400

    commit tree a4d1c441295f41413de6194cb320b8a94c7269cb

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,13 +7,29 @@ class MoviesController < ApplicationController
   end
 
   def show
-    movie = Movie.find(params[:id])
-    render json: movie
+    movie = find_movie
+    render json: movie#.to_json(except: [:created_at, :updated_at])
   end
+
+  def summary
+    movie = find_movie
+    render json: movie, serializer: MovieSummarySerializer
+  end
+  
+  def summaries
+    movies = Movie.all
+    render json: movies, each_serializer: MovieSummarySerializer
+  end
+  
 
   private
 
   def render_not_found_response
     render json: { error: "Movie not found" }, status: :not_found
   end
+
+  def find_movie
+    Movie.find(params[:id])
+  end
+  
 end
